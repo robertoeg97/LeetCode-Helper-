e135532b4f3f2c881a21f4dd5de5ad7f1bd87cef
Roberto Garcia <robertoeg97@gmail.com>
2023-04-25 02:18:39 -0500

    stripped unimportant prefix from problem before asking chatGPT

--- src/contentScript_Leetcode.ts
+++ src/contentScript_Leetcode.ts
@@ -1,14 +1,22 @@
 import { setStorageItem } from './Storage';
 
-function saveProblem() {
+function getProgrammingLanguage(): string {
+  const programmingLanguageDropDownMenu = document.querySelector('.text-xs.text-label-2.dark:text-dark-label-2');
+  const programmingLanguage = programmingLanguageDropDownMenu.textContent;
+  return programmingLanguage;
+}
+
+function saveProblem(): void {
   // Select the text you want to copy
-  const problemText: string = document.querySelector("meta[name='description']").getAttribute("content");
+  let problemText: string = document.querySelector("meta[name='description']").getAttribute("content");
+  problemText = problemText.substring(problemText.indexOf("? ") + 1); //remove ubiquitous prefix: 'Can you solve this real interview question?'
+  //problemText += '\nAnswer in language: ' + getProgrammingLanguage(); //request answer in the detected programming language
 
   //save it in storage for chatGPT tab
   setStorageItem('problemText', problemText);
 }
 
-function openChatGPT() {
+function openChatGPT(): void {
   // Open a new tab to chat.openai.com
   window.open('https://chat.openai.com', '_blank');
 }
